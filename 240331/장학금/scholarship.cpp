5a24023136bff7d53454670ef52220abb5d33f02
6108 <73987879+6108@users.noreply.github.com>
2024-03-31 16:41:11 +0900

    Title: 장학금, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240331/장학금/scholarship.cpp
+++ 240331/장학금/scholarship.cpp
@@ -6,23 +6,15 @@ int main() {
     int mid_test, final_test;
 
     cin >> mid_test >> final_test;
-    if (mid_test >= 95) {
+    if (mid_test >= 90) {
         if (final_test >= 95)
-            cout << 200000;
-        else if (final_test >= 90)
-            cout << 150000;
-        else 
             cout << 100000;
-    }
-    else if (mid_test >= 90) {
-        if (final_test >= 95)
-            cout << 150000;
         else if (final_test >= 90)
-            cout << 100000;
-        else 
             cout << 50000;
+        else 
+            cout << 0;
     }
-    else
+    else 
         cout << 0;
 
     return 0;
