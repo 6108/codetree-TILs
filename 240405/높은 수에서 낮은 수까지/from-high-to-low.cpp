9dca1bb507b34d21173fdf2e519af8e9187c1e5b
6108 <73987879+6108@users.noreply.github.com>
2024-04-05 20:56:25 +0900

    Title: 높은 수에서 낮은 수까지, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240405/높은 수에서 낮은 수까지/from-high-to-low.cpp
+++ 240405/높은 수에서 낮은 수까지/from-high-to-low.cpp
@@ -6,8 +6,14 @@ int main() {
     int a, b;
 
     cin >> a >> b;
-    for (b; b >= a; b--)
-        cout << b << ' ';
-
+    if (a < b) {
+        for (b; b >= a; b--)
+            cout << b << ' ';
+    }
+    else {
+        for (a; a >= b; a--)
+            cout << a << ' ';
+    }
+    
     return 0;
 }
