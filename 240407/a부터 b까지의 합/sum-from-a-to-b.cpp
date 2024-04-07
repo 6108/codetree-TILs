e5cfe4e1926eb70192321b62857c40f33bab5b24
6108 <73987879+6108@users.noreply.github.com>
2024-04-07 13:43:09 +0900

    Title: a부터 b까지의 합, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240407/a부터 b까지의 합/sum-from-a-to-b.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    int sum = 0;
+
+    cin >> a >> b;
+    for (a; a <= b; a++)
+        sum += a;
+    cout << sum;
+
+    return 0;
+}
