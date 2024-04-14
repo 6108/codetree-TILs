84bc398aacaba01a05012411c542c9464242e29a
6108 <73987879+6108@users.noreply.github.com>
2024-04-14 13:20:41 +0900

    Title: 체크 다이아몬드, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240414/체크 다이아몬드/check-diamond.cpp
+++ 240414/체크 다이아몬드/check-diamond.cpp
@@ -20,7 +20,7 @@ int main() {
     for (int i = 1; i < n; i++) {
         for (int j = 0; j < i; j++)
             cout << ' ';
-        for (int j = 0; j < n - i + 1; j++) {
+        for (int j = 0; j < n * 2 - i * 2 ; j++) {
             if (j % 2 == 0)
                 cout << '*';
             else
