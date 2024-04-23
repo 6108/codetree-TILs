0bf0a08983bbad64ab03ea1fe7d54aff94388883
6108 <73987879+6108@users.noreply.github.com>
2024-04-23 14:21:27 +0900

    Title: 완전수, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240423/완전수/perfect-number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int start, end, cnt;
+    int ans = 0;
+
+    cin >> start >> end;
+    for(start; start <= end; start++) {
+        cnt = 0;
+        for (int i = 1; i < start; i++) {
+            if (start % i == 0)
+                cnt += i;
+        }
+        if (cnt == start)
+            ans++;
+    }
+    cout << ans;
+
+    return 0;
+}
