7ac097ff901ad7b78dc564252f6b845bdb889eef
6108 <73987879+6108@users.noreply.github.com>
2024-05-09 20:01:32 +0900

    Title: 더 긴 문자열, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240509/더 긴 문자열/longer-string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string a, b;
+
+    cin >> a >> b;
+    if (a.length() == b.length())
+        cout << "same";
+    else if (a.length() > b.length())
+        cout << a << ' ' << a.length();
+    else
+        cout << b << ' ' << b.length();
+
+
+    return 0;
+}
