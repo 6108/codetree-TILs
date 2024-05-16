5187f830a7a9e0f9b2712c83df666a53340b5227
6108 <73987879+6108@users.noreply.github.com>
2024-05-16 11:07:09 +0900

    Title: 단어로 구분하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240516/단어로 구분하기/separate-words-with-words.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string str;
+    getline(cin, str);
+
+    for (int i = 0; i < str.length(); i++) {
+        if (str[i] == ' ') {
+            cout << '\n';
+            continue;
+        }
+        cout << str[i];
+    }
+
+    return 0;
+}
