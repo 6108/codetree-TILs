485aa4e45e94f4e3b16dad501c7faa997b1aaf21
6108 <73987879+6108@users.noreply.github.com>
2024-05-18 22:39:03 +0900

    Title: 짝수 번째만 거꾸로 출력, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240518/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
+++ 240518/짝수 번째만 거꾸로 출력/print-only-even-numbers-backwards.cpp
@@ -6,10 +6,10 @@ int main() {
     string str;
 
     cin >> str;
-    for (int i = str.length(); 0 < i; i--) {
+    for (int i = str.length(); 0 <= i; i--) {
         if (i % 2)
             cout << str[i];
     }
-    
+
     return 0;
 }
