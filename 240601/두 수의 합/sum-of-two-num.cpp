870e2ffd5402df6da498a88fa05fd59c8ef0cb04
6108 <73987879+6108@users.noreply.github.com>
2024-06-01 17:00:23 +0900

    Title: 두 수의 합, Time: 1045ms, Memory: 4MB, Status: Time Limit Exceed - CodeTree

--- 240601/두 수의 합/sum-of-two-num.cpp
+++ 240601/두 수의 합/sum-of-two-num.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <unordered_map>
 using namespace std;
 
 int main() {
     int n, k;
     cin >> n >> k;
     int cnt = 0;
-    int a[n];
+    int a;
+    unordered_map<int, int> map;
 
     for (int i = 0; i < n; i++) {
-        cin >> a[i];
+        cin >> a;
+        map[i] = a;
     }
     for (int i = 0; i < n; i++) {
         for (int j = i + 1; j < n; j++) {
-            if (a[i] + a[j] == k)
+            if (map[i] + map[j] == k)
                 cnt++;
         }
     }
