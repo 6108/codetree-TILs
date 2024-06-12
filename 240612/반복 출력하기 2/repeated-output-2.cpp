249427daae041057427f27f84d1c88ad3b7e3e0f
6108 <73987879+6108@users.noreply.github.com>
2024-06-12 13:41:07 +0900

    Title: 반복 출력하기 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240612/반복 출력하기 2/repeated-output-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void PrintHelloWorld(int n) {
+    if (n <= 0)
+        return;
+    PrintHelloWorld(n - 1);
+    cout << "HelloWorld\n";
+}
+
+int main() {
+    int n;
+
+    cin >> n;
+    PrintHelloWorld(n);
+    
+    return 0;
+}
