099f324619a88485d171afac051d478954f7ed4d
6108 <73987879+6108@users.noreply.github.com>
2024-09-03 16:04:57 +0900

    Title: 정수만 더하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 240903/정수만 더하기/add-only-integers.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int sum = 0;
+
+    for (int i = 0; i < s.length(); i++) {
+        if ('0' <= s[i] && s[i] <= '9') {
+            sum += s[i] - '0';
+        }
+    }
+    cout << sum;
+
+    return 0;
+}
